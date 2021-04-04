5e0d39da2fb95984c40d0300933a745551936f85
Duarte Morais <duarte.morais@tecnico.ulisboa.pt>
2021-04-04 19:06:53 +0100

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/duarte_33/turtle_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/duarte_33/duarte_ws/devel;/opt/ros/melodic'.split(';') if '/home/duarte_33/duarte_ws/devel;/opt/ros/melodic' != '' else []
